a2749767b75ffb2cbb5035812aa334b5d8eaa4fd
Denis Maksimov <dragonfly-net@users.noreply.github.com>
2020-04-08 14:16:43 +0300

    update AWS_ACCOUNT

--- aws/aws_cli_auth.sh
+++ aws/aws_cli_auth.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 USER="user"
-AWS_ACCOUNT="000000000000"
+# Use AWS Account ID, not name!
+AWS_ACCOUNT_ID="000000000000"
 
 if [ -z "$1" ] ; then
 	echo "Usage: source $0 session-token"
@@ -12,7 +13,8 @@ for var in AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN; do unset $
 
 echo "Check your devices"
 aws iam list-mfa-devices --user-name ${USER}
-codes=$(aws sts get-session-token --serial-number "arn:aws:iam::${AWS_ACCOUNT}:mfa/${USER}" --token-code $1)
+# Just replace string in --serial-number to SerialNumber from command above
+codes=$(aws sts get-session-token --serial-number "arn:aws:iam::${AWS_ACCOUNT_ID}:mfa/${USER}" --token-code $1)
 echo $codes
 export AWS_ACCESS_KEY_ID=$(echo $codes | jq -r .Credentials.AccessKeyId)
 export AWS_SECRET_ACCESS_KEY=$(echo $codes | jq -r .Credentials.SecretAccessKey)
